fa4783d508f9b09ea70720ccb802d4b74cae6f3d
Ben Dror <dan.ben.dror@sap.com>
2024-01-19 22:55:07 +0200

    restructure project

---
+++ src/About.js
@@ -0,0 +1,20 @@
+function About() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <h1><code>react application template </code></h1><i>by</i>  <code><i>dan7467</i></code>
+          <p>
+            <button className="button-18"><b>Home</b></button>&ensp;
+            <button className="button-18"><b>About</b></button>&ensp;
+            <button className="button-18"><b>Contact us</b></button>
+          </p>
+        </header>
+      </div>
+  
+      
+    );
+  }
+  
+  export default About;
+  
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Contact from './Contact';
+import About from './About';
 
 function App() {
   return (
@@ -20,24 +22,3 @@ function App() {
 }
 
 export default App;
-
-export function Contact() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1><code>Contact us!</code></h1>  <code>yourname@email.com</code>
-        </p>
-        <p>
-          <button class="button-18"><b>Home</b></button>&ensp;
-          <button class="button-18"><b>About</b></button>&ensp;
-          <button class="button-18"><b>Contact us</b></button>
-        </p>
-      </header>
-    </div>
-
-    
-  );
-}
-;
