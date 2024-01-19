67e2cfbc44be24ad4705987658c24a3a50395170
Ben Dror <dan.ben.dror@sap.com>
2024-01-19 21:48:41 +0200

    fixed classes in App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,28 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
           <h1><code>react application template </code></h1><i>by</i>  <code><i>dan7467</i></code>
+        <p>
+          <button className="button-18"><b>Home</b></button>&ensp;
+          <button className="button-18"><b>About</b></button>&ensp;
+          <button className="button-18" onClick={Contact}><b>Contact us</b></button>
+        </p>
+      </header>
+    </div>
+
+    
+  );
+}
+
+export default App;
+
+export function Contact() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1><code>Contact us!</code></h1>  <code>yourname@email.com</code>
         </p>
         <p>
           <button class="button-18"><b>Home</b></button>&ensp;
@@ -20,5 +40,4 @@ function App() {
     
   );
 }
-
-export default App;
+;
