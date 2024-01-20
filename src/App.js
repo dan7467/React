d349353998d0574abc82e81be2028f5bcb952f40
Ben Dror <dan.ben.dror@sap.com>
2024-01-20 17:24:50 +0200

    extended App.js with functions Contact, Home and About

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import Contact from './Contact';
-import About from './About';
+
+function Contact() {
+  document.getElementById('header_title').innerText = 'Contact us!';
+  document.getElementById('p1').innerHTML = '<code>yourname@email.com</code>';
+}
+
+function About() {
+  document.getElementById('header_title').innerText = 'About us..';
+  document.getElementById('p1').innerText = 'Add some information or history for your project';
+}
+
+function Home() {
+  document.getElementById('header_title').innerHTML = '<code>react application template</code>';
+  document.getElementById('p1').innerHTML = '<i>by </i><code><i>dan7467</i></code>';
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <h1><code>react application template </code></h1><i>by</i>  <code><i>dan7467</i></code>
-        <p>
-          <button className="button-18"><b>Home</b></button>&ensp;
-          <button className="button-18"><b>About</b></button>&ensp;
+          <h1 id="header_title"><code>react application template </code></h1><p id="p1"><i>by </i><code><i>dan7467</i></code></p>
+        <p id="buttons">
+          <button className="button-18" onClick={Home}><b>Home</b></button>&ensp;
+          <button className="button-18" onClick={About}><b>About</b></button>&ensp;
           <button className="button-18" onClick={Contact}><b>Contact us</b></button>
         </p>
       </header>
     </div>
-
     
   );
 }
