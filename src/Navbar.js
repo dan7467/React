40f8fd4c3a9cec054798272a3c195fbfc4a35c63
Ben Dror <dan.ben.dror@sap.com>
2024-01-21 19:06:03 +0200

    navbar CSS fix

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,13 @@
 import { About, Contact, Home } from "./App";
 
 function Navbar() {
-    return (<div id="c_navbar">
+    return (<div id="c_navbar"><div id="innerdiv">
           <button className="button-18" onClick={Home}><b>Home</b></button>&ensp;&ensp;
           <button className="button-18" onClick={About}><b>About</b></button>&ensp;&ensp;
           <button className="button-18" onClick={Contact}><b>Contact us</b></button>
           <div id="userarea"><div><a href="/Login">Login</a> | <a href="/Signup">Sign-up!</a>&ensp;&ensp;</div></div>
       &ensp;&ensp;
-      
+      </div>
       </div>);
 }
 
